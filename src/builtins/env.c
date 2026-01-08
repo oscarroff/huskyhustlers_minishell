76a1de9a38fe53755343a48505112cb8a45c6f20
Thomas Roff <tommieroff@gmail.com>
2026-01-08 15:31:06 +0200

    feat: rewrite of env() and changes to envp helper functions to get export working

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -13,37 +13,47 @@
 #include "../../inc/minishell.h"
 #include "../../inc/execution.h"
 
-static void print_env(t_tree *tree, t_keyval *env);
+static void print_env(t_tree *tree, char *env);
 
 int env(t_exec *exec)
 {
-    t_keyval    *env;
-    char        *arg;
-    size_t      i; 
-    
+    char    **envp;
+    size_t  i;
+
+    if (!exec)
+        return (FAIL);
+    envp = NULL;
+    if (!envp_export(&envp, exec->tree))
+        return (FAIL);
     i = 0;
-    arg = exec->cmd->argv[1];
-    if (arg)
-    {
-        exec->exec_status = exe_err(exec, arg, (int []){WARN, ERR_CMDNF});
-        return (ERROR);
-    }
-    while (i < exec->tree->envp->len)
-    {
-        env = *(t_keyval **)vec_get(exec->tree->envp, i);
-        if (env->value)
-            print_env(exec->tree, env);
-        i++;
-    }
+    while (envp[i])
+        print_env(exec->tree, envp[i++]);
+    // t_keyval    *env;
+    // char        *arg;
+    // size_t      i; 
+    //
+    // i = 0;
+    // arg = exec->cmd->argv[1];
+    // if (arg)
+    // {
+    //     exec->exec_status = exe_err(exec, arg, (int []){WARN, ERR_CMDNF});
+    //     return (ERROR);
+    // }
+    // while (i < exec->tree->envp->len)
+    // {
+    //     env = *(t_keyval **)vec_get(exec->tree->envp, i);
+    //     if (env->value)
+    //         print_env(exec->tree, env);
+    //     i++;
+    // }
+    // try_write(exec->tree, STDOUT_FILENO, "\n");
     return (0);
 }
 
-static void print_env(t_tree *tree, t_keyval *env)
+static void print_env(t_tree *tree, char *env)
 {
     int         fd_out;
 
     fd_out = STDOUT_FILENO;
-    try_write(tree, fd_out, env->key);
-    try_write(tree, fd_out, "=");
-    try_write_endl(tree, fd_out, env->value);
+    try_write_endl(tree, fd_out, env);
 }
