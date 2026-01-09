82634c889c3fe652f08b93d5882492a92a9f046d
Janne Valkama <janne.e.valkama@gmail.com>
2026-01-09 17:11:54 +0200

    progress on redir handling

--- src/executor/io_redir.c
+++ src/executor/io_redir.c
@@ -13,80 +13,76 @@
 #include "../../inc/minishell.h"
 #include "../../inc/execution.h"
 
-static inline bool  set_redir(t_exec *exec);
-//static inline void  set_heredoc(t_exec *exec);
-//static inline int   set_in_f(t_exec *exec);
-//static inline int   set_out_f(t_exec *exec);
+static bool  set_redir(t_exec *exec, char **in, char **out, char **app);
+static void  set_heredoc(t_exec *exec);
+static int   set_in_file(t_exec *exec, char **in);
+static int   set_out_file(t_exec *exec, char **out, char **app);
 
 void	get_redirs(t_exec *exec)
 {
-    // char    **inputf_chain;
-    // char    **outputf_chain;
+    char    **input_fs;
+    char    **output_fs;
+    char    **append_fs;
 
-    // inputf_chain = exec->cmd->input;
-    // outputf_chain = exec->cmd->output;
-	set_redir(exec);
+    input_fs = exec->cmd->input;
+    output_fs = exec->cmd->output;
+    append_fs =  exec->cmd->append;
+	set_redir(exec, input_fs, output_fs, append_fs);
 }
 
-static inline bool    set_redir(t_exec *exec)
+static bool    set_redir(t_exec *exec, char **in, char **out, char **app)
 {
-    (void) exec;
-    /*
-    if (file doesnt exist:)
-        ": ambiguous redirect"
-        if type == IN
-            redir_in = ERROR;
-        if type == OUT
-            redir_out = ERROR;
-    else
+    if (in[0])
     {
-        if type == IN
-            if set_in_f() == ERROR
-                return false
-        if type == HEREDOC
-            set_in_heredoc()
-        if type == OUT || type == APPEND
-            if set_out_f() == ERROR
-                return false
+        if (set_in_file(exec, in) == ERROR)
+            return (false);
     }
-    */
+    if (exec->cmd->heredoc[0])
+        set_heredoc(exec);
+    if (out[0] || app[0])
+    {
+        if (set_out_file(exec, out, app) == ERROR)
+             return (false);
+    }
     return (true);
 }
 
-/*
-static inline void  set_heredoc(t_exec *exec)
+static void  set_heredoc(t_exec *exec)
 {
     (void) exec;
     //just route IN the data from heredoc tommy implementation
 }
 
-static inline int   set_in_f(t_exec *exec)
+static int   set_in_file(t_exec *exec, char **in)
 {
-    //char    *file;
+    char    *file;
 
-    //file = *exec->cmd->input;
+    file = in[0];
     if (exec->redir_in != STDIN_FILENO && exec->redir_in != ERROR)
         close(exec->redir_in);
-    //exec->redir_in = try_open(exec->tree, file, O_RDONLY, 0);
+    exec->redir_in = try_open(exec->tree, file, O_RDONLY, 0);
     return (exec->redir_in);
 }
 
-static inline int   set_out_f(t_exec *exec)
+static int   set_out_file(t_exec *exec, char **out, char **app)
 {
-    //char    *file;
+    char    *file;
     int     o_flag;
     
-    //file = *exec->cmd->output;
     o_flag = O_WRONLY | O_CREAT;
-
-    //if type == APPEND
-    //    o_flag |= O_APPEND;
-    //if type == OUT
-    //    o_flag |= O_TRUNC;
-
+    if (app[0])
+    {
+        file = app[0];
+        o_flag |= O_APPEND;
+    }
+    if (out[0])
+    {
+        file = out[0];
+        o_flag |= O_TRUNC;
+    }
     if (exec->redir_out != STDOUT_FILENO && exec->redir_out != ERROR)
         close(exec->redir_out);
-    //exec->redir_out = try_open(exec->tree, file, o_flag, RW_RW_RW_);
+    exec->redir_out = try_open(exec->tree, file, o_flag, RW_RW_RW_);
     return (exec->redir_out);
 }
-*/
+
