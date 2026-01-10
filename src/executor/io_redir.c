0092313ac26166e2ffab8c11f73d0cbe6d82f4c7
Janne Valkama <janne.e.valkama@gmail.com>
2026-01-10 15:23:15 +0200

    in and out redirections work

--- src/executor/io_redir.c
+++ src/executor/io_redir.c
@@ -13,33 +13,34 @@
 #include "../../inc/minishell.h"
 #include "../../inc/execution.h"
 
-static bool  set_redir(t_exec *exec, char **in, char **out, char **app);
+static bool  init_redir(t_exec *exec, char **in, char **out, char **app);
 static void  set_heredoc(t_exec *exec);
 static int   set_in_file(t_exec *exec, char **in);
 static int   set_out_file(t_exec *exec, char **out, char **app);
 
 void	get_redirs(t_exec *exec)
 {
-    char    **input_fs;
-    char    **output_fs;
-    char    **append_fs;
+    char    **in_fs;
+    char    **out_fs;
+    char    **app_fs;
 
-    input_fs = exec->cmd->input;
-    output_fs = exec->cmd->output;
-    append_fs =  exec->cmd->append;
-	set_redir(exec, input_fs, output_fs, append_fs);
+    in_fs = exec->cmd->input;
+    out_fs = exec->cmd->output;
+    app_fs =  exec->cmd->append;
+    if (in_fs || out_fs || app_fs || exec->cmd->heredoc)
+	    init_redir(exec, in_fs, out_fs, app_fs);
 }
 
-static bool    set_redir(t_exec *exec, char **in, char **out, char **app)
+static bool    init_redir(t_exec *exec, char **in, char **out, char **app)
 {
-    if (in[0])
+    if (in && in[0])
     {
         if (set_in_file(exec, in) == ERROR)
             return (false);
     }
-    if (exec->cmd->heredoc[0])
+    if (exec->cmd->heredoc && exec->cmd->heredoc[0])
         set_heredoc(exec);
-    if (out[0] || app[0])
+    if ((out && out[0]) || (app && app[0]))
     {
         if (set_out_file(exec, out, app) == ERROR)
              return (false);
@@ -69,13 +70,14 @@ static int   set_out_file(t_exec *exec, char **out, char **app)
     char    *file;
     int     o_flag;
     
+    file = NULL;
     o_flag = O_WRONLY | O_CREAT;
-    if (app[0])
+    if (app && app[0])
     {
         file = app[0];
         o_flag |= O_APPEND;
     }
-    if (out[0])
+    if (out && out[0])
     {
         file = out[0];
         o_flag |= O_TRUNC;
