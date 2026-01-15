811969df8bb8a907e031cc37e58666d4cc871bd0
Thomas Black-Roff <thblack-@c1r4p8.hive.fi>
2026-01-15 17:22:22 +0200

    chore: small typo fixes

--- inc/debugging.h
+++ inc/debugging.h
@@ -6,7 +6,7 @@
 /*   By: thblack- <thblack-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 16:02:09 by thblack-          #+#    #+#             */
-/*   Updated: 2026/01/15 17:03:31 by thblack-         ###   ########.fr       */
+/*   Updated: 2026/01/15 17:09:54 by thblack-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,14 @@
 # include "minishell.h"
 
 // FLAG HANDLING
-int	handle_flags(t_flag *mode_flag, int argc, char **argv);
+int		handle_flags(t_flag *mode_flag, int argc, char **argv);
 
 // PRINTING
-void					print_tokens(t_vec *tokens);
-void					print_tokens_vars(t_vec *tokens);
-void					print_tok_vars(t_token *tok);
-void					print_cmd_tab(t_vec *cmd_tab);
-void					print_debugging(t_vec *tokens, t_tree *tree);
-void					print_envp(t_tree *tree);
-
+void	print_tokens(t_vec *tokens);
+void	print_tokens_vars(t_vec *tokens);
+void	print_tok_vars(t_token *tok);
+void	print_cmd_tab(t_vec *cmd_tab);
+void	print_debugging(t_vec *tokens, t_tree *tree);
+void	print_envp(t_tree *tree);
 
 #endif
